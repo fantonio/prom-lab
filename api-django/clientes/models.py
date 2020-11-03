2d48983da6f1b7894826e4e0d52586b7163ad72a
Fábio Ferreira <fantonios@gmail.com>
2020-11-03 01:52:37 -0300

    Instrumentando metricas de Aplicação

--- api-django/clientes/models.py
+++ api-django/clientes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
 
-class Cliente(models.Model):
+class Cliente(ExportModelOperationsMixin('cliente'), models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False, max_length=30, )
     cpf = models.CharField(max_length=11, unique=True)
--- api-django/setup/settings.py
+++ api-django/setup/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+      #  'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
